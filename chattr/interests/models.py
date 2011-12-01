6207e446f20faf05dd0983b3264cebd07bfd92d8
Sheila Porter <sirocelott@gmail.com>
2011-12-01 02:09:04 -0500

    added logo, updated styles

--- chattr/interests/models.py
+++ chattr/interests/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Interest(models.Model):
-    #name = models.CharField(max_length=200, unique=True)
     name = models.CharField(max_length=200)
     
     def __unicode__(self):
@@ -11,9 +10,6 @@ class Interest(models.Model):
 class UserInterestLink(models.Model):
     user = models.ForeignKey(User)
     interest = models.ForeignKey(Interest)
-    
-    #class Meta:
-        #unique_together = ("user", "interest")
     
     def __unicode__(self):
         return self.interest.name
